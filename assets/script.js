773cdc9647a303ae63f708dfef451aa9e6457086
Justin Hoffman <justinhoffman193@gmail.com>
2021-04-25 21:05:52 -0500

    added color pallet

---
+++ assets/script.js
@@ -0,0 +1,79 @@
+$(function () {});
+  
+/*Variables */
+var today = moment().format("dddd, MMMM Do");
+
+var now = moment().format("H A");
+
+/* Hour Entries */
+var planWorkday = [
+  { time: "9 AM", event: "" },
+  { time: "10 AM", event: "" },
+  { time: "11 AM", event: "" },
+  { time: "12 PM", event: "" },
+  { time: "1 PM", event: "" },
+  { time: "2 PM", event: "" },
+  { time: "3 PM", event: "" },
+  { time: "4 PM", event: "" },
+  { time: "5 PM", event: "" },
+];
+
+/* Local Storage*/
+var workEvents = JSON.parse(localStorage.getItem("workDay"));
+if (workEvents) {
+  planWorkday = workEvents;
+}
+
+
+$("#currentDay").text(today);
+
+
+planWorkday.forEach(function(timeBlock, index) {
+	var timeLabel = timeBlock.time;
+	var blockColor = colorRow(timeLabel);
+	var row =
+		'<div class="time-block" id="' +
+		index +
+		'"><div class="row no-gutters input-group"><div class="col-sm col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">' +
+		timeLabel +
+		'</div><textarea class="form-control ' +
+		blockColor +
+		'">' +
+		timeBlock.event +
+		'</textarea><div class="col-sm col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"><i class="fas fa-save"></i></button></div></div></div>';
+
+	
+	$(".container").append(row);
+});
+
+
+function colorRow(time) {
+	var planNow = moment(now, "H A");
+	var planEntry = moment(time, "H A");
+	if (planNow.isBefore(planEntry) === true) {
+		return "future";
+	} else if (planNow.isAfter(planEntry) === true) {
+		return "past";
+	} else {
+		return "present";
+	}
+}
+
+/* Saving Events */
+$(".saveBtn").on("click", function() {
+	var block = parseInt(
+    $(this)
+			.closest(".time-block")
+			.attr("id")
+	);
+	var userText = $.trim(
+		$(this)
+			.parent()
+			.siblings("textarea")
+			.val()
+	);
+	planWorkday[block].event = userText;
+
+	
+	localStorage.setItem("workDay", JSON.stringify(planWorkday));
+});
